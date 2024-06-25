8f865f9164a926c2a62c8a8e5390bbee28abe654
abletsoff <abletsoff@gmail.com>
2024-06-25 15:45:15 +0300

    Add wrapper around check_if_email_exists

---
+++ check_if_email_exists_wrapper.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+f_verify () {
+        readarray -t emails < $emails_file
+        for email in ${emails[@]}; do
+                result=$(./check_if_email_exists $email)
+                is_reachable=$(echo "$result" | grep '"is_reachable"' | sed "s/,//g" | cut -d ":" -f 2)
+                echo "$email - $is_reachable"
+                sleep 1
+        done
+}
+
+emails_file=$1
+f_verify
