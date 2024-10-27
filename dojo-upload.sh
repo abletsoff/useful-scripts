a766380a1e81abcd3e480aef573f42b6e777f82f
Andrew Abletsov <abletsoff@gmail.com>
2024-10-27 13:09:32 +0300

    dojoupload adjustments

--- dojo-upload.sh
+++ dojo-upload.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-supported_tools=('Burp Scan' 'ZAP Scan' 'Nuclei Scan' 'Wpscan' 'Wpscan API Scan')
-base_url="http://localhost:8080/api/v2"
-auth_header="Authorization: Token $DEFECTDOJO_API_KEY_LOCAL"
+supported_tools=('Burp Scan' 'ZAP Scan' 'Nuclei Scan' 'Wpscan' 'Wpscan API Scan' 'OpenVAS Parser' 'Ubuntu OVAL')
+base_url="$DEFECTDOJO_API_URL"
+auth_header="Authorization: Token $DEFECTDOJO_API_KEY"
 
 f_print_help () {
     echo -e " DAST results upload to DefectDojo\n\n" \
@@ -23,25 +23,27 @@ f_print_tools () {
 
 f_handle_error () {
     message="$1"
+    information="$2"
     echo -e " Error: $message\n"
+    echo "$information"
     f_print_help
 }
 
 f_get_product_id () {
     product_name_encode=$(echo "$product_name" | sed 's/ /%20/g')
-    response=$(curl -s "$base_url/products/?name=$product_name_encode" \
+    response=$(curl -s -k "$base_url/products/?name=$product_name_encode" \
         -H "$auth_header")
     product_id=$(echo "$response" | grep -o -P '"id":\d+' | cut -d ':' -f2)
 
     if [[ $(echo "$response" | grep -P '^{"count":1') == '' ]]; then
-        f_handle_error "Product '$product_name' does not exist"
+        f_handle_error "Unable to get product id" "$response"
         exit 1
     fi
 }
 
 f_create_engagement () {
     engagement_name="$(echo "$tool_name" | sed 's/ /_/g')_$(date '+%Y-%m-%d')"
-    response=$(curl -s -X POST "$base_url/engagements/" \
+    response=$(curl -s -k -X POST "$base_url/engagements/" \
         -H "$auth_header" -H "Content-Type: application/json" \
         -d "{ 
           \"name\": \"${engagement_name}\",
@@ -53,12 +55,12 @@ f_create_engagement () {
       engagement_id=$(echo "$response" | grep -o -P '"id":\d+' | cut -d ':' -f2)
     
     if [[ $engagement_id == '' ]]; then
-        f_handle_error "Unable to create engagement"
+        f_handle_error "Unable to create engagement" "$response"
     fi      
 }
 
 f_upload_scan () {
-    curl -s -X POST "$base_url/import-scan/" \
+    curl -s -k -X POST "$base_url/import-scan/" \
         -H "$auth_header" -H "Content-Type: multipart/form-data" \
         -F "scan_date=$(date '+%Y-%m-%d')" -F "engagement=$engagement_id" \
         -F "scan_type=$tool_name" \
@@ -76,7 +78,7 @@ f_tool_name () {
     done
 
     if [[ "$tool_name" == '' ]]; then
-        f_handle_error "'$tool_slang' is unsupported tool.\n"
+        f_handle_error "'$tool_slang' is unsupported tool.\n" ""
     fi
 }
 
@@ -94,7 +96,7 @@ while getopts "hf:t:p:e:l" opt; do
 done
 
 if [[ $filename == '' || $tool_slang == '' || $product_name == '' ]]; then
-    f_handle_error "Not all arguments"
+    f_handle_error "Not all arguments" ""
 fi
 
 f_tool_name
