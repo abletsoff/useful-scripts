76d79b886c604dc81643f62e2287521fa47c300a
Andrew Abletsov <abletsoff@gmail.com>
2025-02-06 15:30:22 +0300

    sub-ip-organizer

--- sub-ip-organizer.py
+++ sub-ip-organizer.py
@@ -3,15 +3,26 @@
 import os
 import sys
 import re
+import argparse
 
 ipv4_pattern = r'\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b'
-domains = sys.argv[1:]
 dictionary = {}
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--domains', nargs='+', type=str, help="Domains")
+parser.add_argument('--addresses', nargs='+', type=str, help="Addresses files")
+args = parser.parse_args()
+domains = args.domains
+if not domains:
+    domains = []
 
 for path, folders, files in os.walk(os.getcwd()):
     for filename in files:
         with open(os.path.join(os.getcwd(), filename)) as f:
             for line in f.readlines():
+                # Exclude spf entry
+                if "	TXT	" in line:
+                    continue
                 for domain in domains:
                     domain_regex= r'[a-z,0-9,\.,\-]*' + domain
                     domain_matches = re.findall(domain_regex,line)
@@ -23,6 +34,22 @@ for path, folders, files in os.walk(os.getcwd()):
                             else:
                                 if domain_match not in dictionary[ip]:
                                     dictionary[ip].append(domain_match)
+
+
+ip_addresses = []
+
+filenames = args.addresses
+for filename in filenames:
+    with open(filename) as f:
+        for line in f.readlines():
+            ip_matches = re.findall(ipv4_pattern, line.strip('\n'))
+            for ip in ip_matches:
+                if ip not in ip_addresses:
+                    ip_addresses.append(ip)
+
+for ip in ip_addresses:
+    if ip not in dictionary.keys():
+        dictionary[ip] = ''
 
 for key, value in sorted(dictionary.items()):
     print(key)
