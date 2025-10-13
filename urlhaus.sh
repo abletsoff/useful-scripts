463b04803df68f1a2aea67ca30c7a7419459ece6
Andrei Abletsov <johndoe@example.com>
2025-10-13 19:29:48 +0300

    Add URLhause exclusions

--- urlhaus.sh
+++ urlhaus.sh
@@ -7,7 +7,7 @@ declare -A seen seen_full
 # Define exclusions here
 EXCLUDE=("gitlab.com:443" "docs.google.com:443" "drive.google.com:443" "cdn.jsdelivr.net:443"
         "raw.githubusercontent.com:443" "storage.googleapis.com:443" "gist.githubusercontent.com:443"
-        "res.cloudinary.com:443")
+        "res.cloudinary.com:443" "img1.wsimg.com:443" "sites.google.com:443")
 
 while IFS= read -r url || [[ -n $url ]]; do
     url=${url//$'\r'/}           # remove CR
@@ -23,4 +23,4 @@ while IFS= read -r url || [[ -n $url ]]; do
 done < urlhaus.txt
 
 { for k in "${!seen_full[@]}"; do printf '"%s":\n' "$k"; done
-  for k in "${!seen[@]}"; do printf '"%s":\n' "$k"; done; } | sort -u
+  for k in "${!seen[@]}"; do printf '"%s":\n' "$k"; done; } | sort - 
